e933b31bec05a94660ca52737bb12e4d9ee4ba6e
nirakar.asha <nirakar.asha@gmail.com>
2024-08-17 18:19:42 +0530

    add JdbcUserDetailsManager instead of InMemoryUserDetailsManager

--- src/main/java/com/ashan/example/spring/SecurityConfiguration.java
+++ src/main/java/com/ashan/example/spring/SecurityConfiguration.java
@@ -1,18 +1,20 @@
 package com.ashan.example.spring;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,15 +23,16 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) ->
-                requests.requestMatchers(PathRequest.toH2Console()).permitAll()
+                requests.requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated());
-        http.csrf(csrf -> csrf
-                .ignoringRequestMatchers(PathRequest.toH2Console())
-                .disable());
-        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
         http.httpBasic(withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
@@ -41,6 +44,10 @@ public class SecurityConfiguration {
         UserDetails user = User.withUsername("user").password("{noop}user123").roles("USER").build();
         UserDetails admin = User.withUsername("admin").password("{noop}admin123").roles("ADMIN").build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
     }
 }
