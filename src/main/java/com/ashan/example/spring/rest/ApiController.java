d512d7ae3d816eab05051e57e134129c18516365
nirakar.asha <nirakar.asha@gmail.com>
2024-08-17 16:21:14 +0530

    enable api level security based on role

--- src/main/java/com/ashan/example/spring/rest/ApiController.java
+++ src/main/java/com/ashan/example/spring/rest/ApiController.java
@@ -1,5 +1,6 @@
 package com.ashan.example.spring.rest;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
@@ -10,11 +11,13 @@ public class ApiController {
         return "<h1>Welcome from server!!</h1>";
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/user")
     public String userApi() {
         return "<h1>Welcome User!!</h1>";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String adminApi() {
         return "<h1>Welcome Admin!!</h1>";
